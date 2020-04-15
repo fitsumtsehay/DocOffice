251f99d3bd91f3a2c0ae1306600007d62b187f73
fitsumtsehay <tsehayfitsum@gmail.com>
2020-04-15 03:50:22 -0400

    final

---
+++ src/DocOffice.java
@@ -0,0 +1,17 @@
+public class DocOffice {
+    public static void main(String[] args) {
+        String s1 = "PatientDetails";
+        String patientName = "StaceyManson";
+        String hasInsurance = "false";
+        double temp = 102;
+        int numVisits = 5;
+
+        System.out.println("PatientDetails:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
+    }
+
+}
+
